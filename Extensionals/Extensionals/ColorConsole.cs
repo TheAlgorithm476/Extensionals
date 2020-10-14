9dcab1d8aac5996f0d94dafb8810992063c532f6
Brent Van den Abbeel <brent.vandenabbeel@student.romerocollege.be>
2020-10-14 11:37:34 +0200

    Added WriteLineInCOLOR(string format) to ColorConsole.*

---
+++ Extensionals/Extensionals/ColorConsole.cs
@@ -0,0 +1,165 @@
+﻿/*
+ * MIT License
+ *
+ * Copyright (c) 2020 TheAlgorithm476
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+
+namespace Extensionals {
+    public class ColorConsole {
+        /* START Black */
+        public static string WriteLineInBlack(string format) {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END Black */
+        /* START Blue */
+        public static string WriteLineInBlue(string format) {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END Blue */
+        /* START Cyan */
+        public static string WriteLineInCyan(string format) {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END Cyan */
+        /* START DarkBlue */
+        public static string WriteLineInDarkBlue(string format) {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END DarkBlue */
+        /* START DarkGray */
+        public static string WriteLineInDarkGray(string format) {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END DarkGray */
+        /* START DarkGreen */
+        public static string WriteLineInDarkGreen(string format) {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END DarkGreen */
+        /* START DarkMagenta */
+        public static string WriteLineInDarkMagenta(string format) {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END DarkMagenta */
+        /* START DarkRed */
+        public static string WriteLineInDarkRed(string format) {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END DarkRed */
+        /* START DarkYellow */
+        public static string WriteLineInDarkYellow(string format) {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END DarkYellow */
+        /* START Gray */
+        public static string WriteLineInGray(string format) {
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END Gray */
+        /* START Green */
+        public static string WriteLineInGreen(string format) {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END Green */
+        /* START Magenta */
+        public static string WriteLineInMagenta(string format) {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END Magenta */
+        /* START Red */
+        public static string WriteLineInRed(string format) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END Red */
+        /* START White */
+        public static string WriteLineInWhite(string format) {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+        
+        /* END White */
+        /* START Yellow */
+        public static string WriteLineInYellow(string format) {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(format);
+            Console.ResetColor();
+            return format;
+        }
+
+        /* END Yellow */
+    }
+}
